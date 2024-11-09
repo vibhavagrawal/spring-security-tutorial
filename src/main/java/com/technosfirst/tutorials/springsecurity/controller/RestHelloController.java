c3aee333f2508c9b34417a0dfe684a45a318858f
Technos First <technosfirst@gmail.com>
2024-11-09 07:29:57 -0500

    Added method level authorization In this, had to set SecurityConfig as `EnableMethodSecurity`. and then we can annotate a method with PreAuthorize("hasRole('ADMIN')") tag.

--- src/main/java/com/technosfirst/tutorials/springsecurity/controller/RestHelloController.java
+++ src/main/java/com/technosfirst/tutorials/springsecurity/controller/RestHelloController.java
@@ -2,6 +2,7 @@ package com.technosfirst.tutorials.springsecurity.controller;
 
 import com.technosfirst.tutorials.springsecurity.security.UserPrincipal;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,5 +26,11 @@ public class RestHelloController {
     @GetMapping("/admin")
     public String admin(@AuthenticationPrincipal UserPrincipal principal) {
         return String.format("Hello, %s! Your user id is %s. You see this as you are an admin!", principal.getEmail(), principal.getUserId());
+    }
+
+    @GetMapping("/anotheradmin")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String anotheradmin(@AuthenticationPrincipal UserPrincipal principal) {
+        return String.format("Hello, %s! Your user id is %s. You see this another endpoint only if you are an admin!", principal.getEmail(), principal.getUserId());
     }
 }
