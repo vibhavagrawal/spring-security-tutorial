629f3e89717787f112de46f9e78c9a2b8974f2ee
Technos First <technosfirst@gmail.com>
2024-11-08 21:20:48 -0500

    Added an API endpoint for HelloWorld. Also, renamed app.properties to app.yaml & added a postman collection

---
+++ src/main/java/com/technosfirst/tutorials/springsecurity/controller/RestHelloController.java
@@ -0,0 +1,17 @@
+package com.technosfirst.tutorials.springsecurity.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class RestHelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
